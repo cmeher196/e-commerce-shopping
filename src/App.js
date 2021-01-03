9f1a0cba846bf1ccb86456ace10e73875672537c
Chandra Meher <chamehe1@in.ibm.com>
2021-01-03 18:08:33 +0530

    added form input component and integrated with sign in component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import HomePage from './pages/homepage/homepage.component'
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
+import SignInAndSignUp from './pages/signin-and-signup/signin-and-signup.component';
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
